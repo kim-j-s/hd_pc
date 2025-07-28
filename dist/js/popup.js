ac461e51b7e14b970907806f2f0004ad54951dbc
mnk <storrymn@gmail.com>
2025-07-28 17:00:44 +0900

    dist

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -8,6 +8,8 @@
  * @param {string} target [필수]Open할 popup id
  * @returns {void}
  */
+
+let openScrollTop = null;
 function openHDPopup($triggerEl, target) {
 	if (target == undefined || target == null) {
 		console.log("오픈할 팝업 타겟이 없습니다.");
@@ -43,30 +45,48 @@ function openHDPopup($triggerEl, target) {
 
 	$target.attr("opner", openerId);
 
-	if ($(".wrap").attr("aria-hidden") == undefined || $(".wrap").attr("aria-hidden") == "false") {
-		$(".wrap").attr("aria-hidden", "true");
-		$(".wrap").attr("inert", "");
-	}
+	// if ($(".wrap").attr("aria-hidden") == undefined || $(".wrap").attr("aria-hidden") == "false") {
+	// 	$(".wrap").attr("aria-hidden", "true");
+	// 	$(".wrap").attr("inert", "");
+	// }
 
 	// popup active
-	$("body").css("overscroll-behavior", "contain");
-	$("body").addClass("scroll_lock");
-	$target.addClass("active");
+	// $("body").css("overscroll-behavior", "contain");
+	// $("body").addClass("scroll_lock");
+	// $target.addClass("active");
 
-	// bottom 팝업일 경우 - drag
-	if ($target.hasClass("bottom")) {
-		draggable($target);
+	// scrollTop 저장
+  openScrollTop = $(window).scrollTop();
+
+	// console.log('마지막 저장 위치 : ', openScrollTop);
+
+	//팝업 열기
+	$('#wrap').addClass('scroll_lock');
+	$target.addClass('active');
+
+	// scrollTop 복원
+	if (openScrollTop !== null) {
+		// $(window).scrollTop(openScrollTop);
+		$('.wrap').scrollTop(openScrollTop);
+		lastScrollTop = null; // 초기화
 	}
 
-	$('.popup_cont').animate({
-		scrollTop: 0 // 팝업 열 때 스크롤을 최상단으로 이동
-	}, 0);
+	// bottom 팝업일 경우 - drag
+	// if ($target.hasClass("bottom")) {
+	// 	draggable($target);
+	// }
 
-	$target.attr("aria-hidden", "false");
-	$target.find(".popup_inner").attr({
-		tabindex: 0,
-		"aria-hidden": "false",
-	});
+	if($content) {
+		$content.animate({
+			scrollTop: 0 // 팝업 열 때 스크롤을 최상단으로 이동
+		}, 0);
+	}
+
+	// $target.attr("aria-hidden", "false");
+	// $target.find(".popup_inner").attr({
+	// 	tabindex: 0,
+	// 	"aria-hidden": "false",
+	// });
 
 	if ($header) $header.attr("tabindex", 0);
 	if ($content) $content.attr("tabindex", 0);
@@ -81,18 +101,24 @@ function openHDPopup($triggerEl, target) {
 	}
 
 	setTimeout(() => {
-		if (focusTarget) {
-			focusTarget.focus();
-			focusTarget.attr("aria-live", "assertive"); //포커스 이동을 스크린 리더에 알림
+		// if (focusTarget) {
+		// 	focusTarget.focus();
+		// 	focusTarget.attr("aria-live", "assertive"); //포커스 이동을 스크린 리더에 알림
 
-			setTimeout(() => {
-				focusTarget.attr("aria-live", null);
-			}, 0);
-		}
-		if (activePopups.length > 0) {
-			activePopups.attr("aria-hidden", "true");
-			activePopups.find(".popup_inner").attr("aria-hidden", "true");
-		}
+		// 	setTimeout(() => {
+		// 		focusTarget.attr("aria-live", null);
+		// 	}, 0);
+		// }
+		// if (activePopups.length > 0) {
+		// 	activePopups.attr("aria-hidden", "true");
+		// 	activePopups.find(".popup_inner").attr("aria-hidden", "true");
+		// }
+		setTimeout(function(){
+			//팝업 내부에 tabindex 설정
+			setTabindex($target);
+			//팝업 내부에서 focus순환
+			focusTrap($target);
+		},100);
 	}, 350);
 }
 
@@ -103,6 +129,7 @@ function openHDPopup($triggerEl, target) {
  * @param {object} [returnTarget] [선택]팝업이 닫힌 뒤에 포커스할 엘리먼트
  * @returns {void}
  */
+let closeScrollTop = null;
 function closeHDPopup(target, returnTarget = null) {
 	if (!target) {
 		console.error("close할 팝업 id를 지정해주세요");
@@ -159,12 +186,24 @@ function closeHDPopup(target, returnTarget = null) {
 			$target.find(".popup_inner").attr("aria-hidden", "true").removeAttr("tabindex", 0);
 		}, 350);
 	} else {
-		$(".wrap").attr("aria-hidden", "false");
-		$(".wrap").removeAttr("inert");
-		$("body").css("overscroll-behavior", "auto");
-		$("body").removeClass("scroll_lock");
-		$target.attr("aria-hidden", "true");
-		$target.find(".popup_inner").attr("aria-hidden", "true").removeAttr("tabindex", 0);
+		// $(".wrap").attr("aria-hidden", "false");
+		// $(".wrap").removeAttr("inert");
+		// $("body").css("overscroll-behavior", "auto");
+		// $("body").removeClass("scroll_lock");
+		// $target.attr("aria-hidden", "true");
+		// $target.find(".popup_inner").attr("aria-hidden", "true").removeAttr("tabindex", 0);
+
+		closeScrollTop = $('.wrap').scrollTop();
+		console.log('팝업 닫기 전 위치 : ', closeScrollTop);
+
+		//팝업이 모두 닫힌 경우
+		$('#wrap').removeClass('scroll_lock');
+		if (closeScrollTop !== null) {
+			$(window).scrollTop(closeScrollTop);
+			// $('.wrap').scrollTop(closeScrollTop);
+			closeScrollTop = null; // 초기화
+		}
+		$target.find(".popup_inner").removeAttr("tabindex").off('keydown.focusTraversal');
 
 		const focusTarget = $triggerEl || $("body");
 
@@ -188,9 +227,9 @@ function generateUUID() {
 
 /* Toast 팝업 */
 let toastTimer = null;
-function toastAction(click) {
-	const $toast = $(".toast_wrap"),
-		msg = $(click).data("msg");
+function toastAction(target, msg) {
+	const $toast = $(".toast_wrap");
+		//msg = $(target).data("msg");
 
 	if (toastTimer != undefined) return;
 	// console.log(toastTimer);
@@ -210,4 +249,43 @@ function toastAction(click) {
 function toastMsg(msg) {
 	// const text = $('<div class='toast_msg'></div>').text(msg);
 	$(".toast_msg").text(msg);
+}
+
+//팝업 내부에 tabindex 설정
+function setTabindex($target){
+	$target.find('.popup_inner').attr('tabindex', '0').focus();
+
+			const $header = $target.find('.popup_head_title');
+			const $content = $target.find('.popup_cont');
+			if($header) {
+				const title = $header.text().trim();
+				if(title.length>0){
+					$header.attr('tabindex',0);
+				}
+			}
+			if($content){ $content.attr('tabindex',0); }
+}
+
+
+//팝업 내부에서 focus순환
+function focusTrap($target){
+	$target.find('.popup_inner, .event_pop_ele_inner').off('keydown.focusTraversal').on('keydown.focusTraversal', function(e) {
+		if (e.key === 'Tab') {
+			const focusableEle = $target.find('button, input, select, textarea, a, .popup_inner').filter(':not([disabled])'); // 포커스 가능한 요소들만
+			const firstEle = focusableEle.first();
+			const lastEle = focusableEle.last();
+			
+			if (e.shiftKey) {
+				if (document.activeElement === firstEle[0]) {
+					lastEle.focus();
+					e.preventDefault();
+				}
+			} else {
+				if (document.activeElement === lastEle[0]) {
+					firstEle.focus();
+					e.preventDefault();
+				}
+			}
+		}
+	});
 }
