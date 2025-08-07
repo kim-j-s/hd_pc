87c5cc4a96e20a0969be94a7a1ee29cafbffdf9a
mnk <storrymn@gmail.com>
2025-08-07 19:07:19 +0900

    메인 관련 수정

--- public/js/common_pc.js
+++ public/js/common_pc.js
@@ -44,21 +44,24 @@ $(function() {  // DOM이 준비되었을 때 실행
 	});
 
 	// 헤더 이벤트 스크립트
-	let lastScrollTop = 0;
-  $(window).on('scroll', function() {
-    let st = $(this).scrollTop();
-		// console.log(st);
+	if(!$('.main_cont_wrap').length){
+		let lastScrollTop = 0;
+		
+		$(window).on('scroll', function() {
+			let st = $(this).scrollTop();
+			// console.log(st);
 
-    if (st > lastScrollTop) {
-      // 아래로 스크롤
-      $('body').removeClass('sc_up').addClass('sc_down');
-    } else if (st < lastScrollTop) {
-      // 위로 스크롤
-      $('body').removeClass('sc_down').addClass('sc_up');
-    }
-
-    lastScrollTop = st;
-  });
+				if (st > lastScrollTop) {
+					// 아래로 스크롤
+					$('body').removeClass('sc_up').addClass('sc_down');
+				} else if (st < lastScrollTop) {
+					// 위로 스크롤
+					$('body').removeClass('sc_down').addClass('sc_up');
+				}
+		
+				lastScrollTop = st;
+		});
+	}
 
 	// 헤더 gnb
 	$DOM.on('mouseenter', '.nav_list > li', function() {
