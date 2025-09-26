636234da01adf1e58b20a078c3588302197db8ec
hyun <hyun@naruint.com>
2025-09-26 13:47:27 +0900

    카드유효기간 표시

--- public/js/common.js
+++ public/js/common.js
@@ -152,6 +152,14 @@
 				$this.val(newVal).removeClass('isVal');
 			}
 		}
+		//카드유호기간
+		if( $this.closest('.input_text').hasClass('expiry_date') && !$this.prop('readonly') && !$this.prop('disabled') ){
+			if(val){
+				const newVal = val.replace(/\s*\/\s*/g, '');
+				$this.attr('maxlength', 4);
+				$this.val(newVal).removeClass('isVal');
+			}
+		}
     
   }).on('blur', '.inp > input', function(){
     const $this = $(this),
@@ -176,6 +184,17 @@
 			if(val){
 				val = val.replace(/[^0-9*]/g, '');
 				newVal = ' - ' + val.replace(/([0-9*]{4})(?=[0-9*])/g, '$1 - ');
+				$this.val(newVal).addClass('isVal');
+			}else {
+				$this.removeClass('isVal');
+			}
+		}
+		// 카드유호기간
+		if( $this.closest('.input_text').hasClass('expiry_date') && !$this.prop('readonly') && !$this.prop('disabled') ){
+			$this.attr('maxlength', 4);
+			if(val){
+				val = val.replace(/[^0-9*]/g, '');
+				newVal = val.replace(/([0-9*]{2})(?=[0-9*])/g, '$1 / ');
 				$this.val(newVal).addClass('isVal');
 			}else {
 				$this.removeClass('isVal');
